5360c1c4e1a7a2add63d503e9dbb324f6622766b
Jorge Gómez <syvic@sindormir.net>
2017-05-17 14:09:51 +0200

    Update tema6b.ino
    Simplificado código

--- tema6b.ino
+++ tema6b.ino
@@ -1,33 +1,25 @@
 #include <TimerOne.h>
-#define PARPADEO_OK   1000000
-#define PARPADEO_ERR  50000
-#define LED 11
+#define PARPADEO_OK   10000
+#define LED 13
 
 void setup() {
   pinMode(LED, OUTPUT);
-  Timer1.initialize(100); // Inicializamos Temporizador 1 a 1s.
-  Timer1.attachInterrupt(parpadea); // Función asociada al desbordamiento.
-  Serial.begin(9600);
+  Timer1.initialize(100);       // Inicializamos Temporizador 1 a 100us.
+  Timer1.attachInterrupt(parpadea);   // Función asociada al desbordamiento.
 }
 
 void parpadea() {
   static unsigned long contador=0;
   contador++;
-  if (contador > PARPADEO_OK / 100){
+  if (contador > PARPADEO_OK){
     contador=0;
-    digitalWrite(11, !digitalRead(11));
+    digitalWrite(LED, !digitalRead(LED));
   }
 }
 
 void loop() {
   for (int i = 0; i < 1023; i++) {
     Timer1.pwm(9, i);
-    delay(1);
-  }
-  if (Serial.available() > 0) {
-    if (Serial.read() == 'x')
-      Timer1.setPeriod(PARPADEO_ERR);
-    else
-      Timer1.setPeriod(PARPADEO_OK);
+    delay(2);
   }
 } 
